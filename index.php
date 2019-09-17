9dec41eb3885e6262801d0ce4403ffc432a8d8d8
Frederic Egele <frederic.egele.67@gmail.com>
2019-09-17 09:34:03 +0200

    association repair cafe

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Repair café</title>
+</head>
+<body>
+
+<h1 class="titre">Repair café</h1>
+<section>
+
+    <article class="informations">
+        <h2 class="titre">Informations</h2>
+        <img src="https://via.placeholder.com/150"></img>
+        <p>Un Repair Café est un lieu d'échange, de partage et surtout ... de réparation ! Vélo déréglé, bouilloire fatiguée, jean troué ou encore vieil ordinateur peuvent y être réparés !</p>
+        <p>Le repair Café est tenu par des bénévoles, l'idée n'est donc pas de créer un atelier "de service après-vente", mais de créer du lien par un échange de savoir, apprendre à réparer soi-même, connaître les petits gestes d'entretien qui retardent la panne.</p>
+    </article>
+
+    <article class="descriptions">
+        <h2 class="titre">Descriptions</h2>
+        <p>Nous ne disposons pas de lieux pour nos manifestations. Nous comptons sur les partenariats avec des structures locales, associations ou centres, pour organiser nos ateliers de réparation.
+            <br>Pour toucher le plus grand public, nos ateliers ont lieu essentiellement les samedis après-midi, de 14h à 17h, et nous sommes en mesure d'en organiser plusieurs par mois, en fonction des disponibilités de nos bénévoles.</p>
+        <p>Nous intervenons principalement dans le rayon de Strasbourg Eurométropole :</p>
+        <p>Achenheim - Breuschwickersheim - Bischheim - Blaesheim - Eckbolsheim - Eckwersheim - Entzheim - Eschau - Fegersheim - Geispolsheim - Hangenbieten - Hoenheim - Holtzheim - Illkirch-Graffenstaden - Kolbsheim - Lampertheim - La Wantzenau - Lipsheim - Lingolsheim - Mittelhausbergen - Mundolsheim - Niederhausbergen - Oberhausbergen - Oberschaeffolsheim - Osthoffen - Ostwald - Plobsheim - Reichstett - Schiltigheim - Souffelweyersheim - Strasbourg - Vendenheim - Wolfisheim</p>
+        <p>Nous recherchons toujours des bénévoles, et nous sommes à l'écoute pour toute proposition de lieu où poser nos instruments.</p>
+    </article>
+
+</section>
+</body>
+</html>
