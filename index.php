8ecb7748a5396493556cb8c59bf097b8908c8caf
Frederic Egele <frederic.egele.67@gmail.com>
2019-09-17 16:18:57 +0200

    legere modif

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <header>
     <nav class="bar">
         <ol>
+            <li class="navbar"><a href="#home"><img class="logo" src="image/logo.png" alt="logo"></a></li>
             <li class="navbar"><a href="#actions">ACTIONS</a></li>
             <li class="navbar"><a href="#associations">ASSOCIATIONS</a></li>
             <li class="navbar"><a href="#Dechets">DECHETS</a></li>
@@ -32,13 +33,19 @@
 
 
     <article class="informations">
-        <img src="https://via.placeholder.com/150" alt="">
+        <img alt="logo recyclage" src="https://cdn.pixabay.com/photo/2018/04/05/23/59/nature-3294632_960_720.jpg">
         <h2 class="titre">Conseils</h2>
-        <p>jgrjjrehguresnof,ksreng,jselgtfnbslrjeqk,nvhjdkrhyjvsuyssefze v' tg e( g(er hg(r (z sthg(h s hstrhs gq q,sdc,uhynisjut</p>
+
+            <li>Le recyclage commence déjà par un tri sélectif de nos ordures ménagères et de nos déchets : piles, cartouche d'encre, papier, verre, plastique en tenant compte de différentes consignes.</li>
+            <li>Je peux aussi déposer mes déchets et mes encombrants à la déchetterie proche de chez moi, comme : les restes de peinture, de solvants usagés ou d'huiles usagées.</li>
+            <li>Je proscris les dépôts sauvages pour préserver mon environnement.</li>
+            <li>Je rapporte à mon pharmacien les médicaments non utilisés ou périmés ainsi que leurs emballages.</li>
+            <li>Rapporter mes objets dont je n'ai plus l'utilité ou arrivant en fin de vie permet de recycler les matières qui les composent.</li>
+
     </article>
 
     <article class="descriptions">
-        <img src="https://via.placeholder.com/150" alt="">
+        <img alt="Poubelle" src="https://cdn.pixabay.com/photo/2013/11/09/20/19/trash-207996_960_720.jpg">
         <h2 class="titre">Points de collecte</h2>
         <p>jgrjjrehguresnof,ksreng,jselgtfnbslrjeqk,nvhjdkrhyjvsuyssefze v' tg e( g(er hg(r (z sthg(h s hstrhs gq q,sdc,uhynisjut</p>
     </article>
