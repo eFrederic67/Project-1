467697c2c40e7879aff7a545e0ae06591e4c0db2
Edouard Neveu <edouard.neveu67@gmail.com>
2019-09-18 16:55:25 +0200

    commit de Edouard

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Green City</title>
+</head>
+
+<body>
+<header>
+    <div class="logonav">
+        <div class="logo"><a href="#home"><img class="logo" src="images/logo.png" alt="logo"></a></div>
+        <nav class="bar">
+
+            <ol>
+                <li class="navbar"><a href="#Actions">ACTIONS</a></li>
+                <li class="navbar"><a href="#Associations">ASSOCIATIONS</a></li>
+                <li class="navbar"><a href="#Dechets">DECHETS</a></li>
+            </ol>
+        </nav>
+    </div>
+    <div id="slider">
+        <figure>
+            <img src="https://cdn.pixabay.com/photo/2019/09/11/13/47/grass-4468766_1280.jpg" alt="">
+            <img src="https://cdn.pixabay.com/photo/2019/09/12/01/14/niagara-falls-4470219_1280.jpg" alt="">
+            <img src="https://cdn.pixabay.com/photo/2019/09/08/10/41/landscape-4460685_1280.jpg" alt="">
+            <img src="https://cdn.pixabay.com/photo/2019/09/14/10/42/bee-friend-4475693_1280.jpg" alt="">
+            <img src="https://cdn.pixabay.com/photo/2019/09/10/16/15/sea-4466575_1280.jpg" alt="">
+        </figure>
+    </div>
+</header>
+
+<section id="Actions">
+    <h2>Actions Eurométropole</h2>
+    <ul class="list-style-none">
+        <!-- action 1 -->
+        <li>
+            <h3>Compostage</h3>
+            <a href="https://www.strasbourg.eu/composter-dechets-compostage-dechets-verts" target="_blank" title="Strasbourg-compostage"><img src="images/compostage.jpeg" alt="Strasbourg-compostage"/></a>
+            <p><br>Actions de compostage accompagnées par Eurométropole</p>
+        </li>
+        <!-- action 2 -->
+        <li>
+            <h3>Eco quartier Danube</h3>
+            <a href="https://www.strasbourg.eu/eco-quartier-danube" target="_blank" title="Eco-quartier Danube"><img src="images/Danube.jpeg" alt="Eco-quartier Danube"/></a>
+            <p><br>Le projet de l'éco-quartier Danube au Neudorf accompagné par Eurométropole</p>
+        </li>
+        <!-- action 3 -->
+        <li>
+            <h3>Economie Circulaire / Recyclage</h3>
+            <a href="https://www.economiecirculaire.org/company/h/eurometropole-de-strasbourg.html" target="_blank" title="Strasbourg-eco-circulaire"><img class="image-droite" src="images/eco-circulaire.jpeg" alt="Strasbourg-eco-circulaire"/></a>
+            <p>Actions autour de l'économie circulaire et du recyclage accompagnées par Eurométropole</p>
+        </li>
+    </ul>
+</section>
+<section id="Associations">
+    <h2>Associations</h2>
+    <div class="container">
+        <article id="Envie">
+            <div class="overlay">
+                <h4>Envie</h4>
+                <p>créée à Strasbourg, cette association a maintenant plein de magasins</p>
+                <a class="button-assoc"
+                   href="envie.php">Plus d'infos</a>
+            </div>
+        </article>
+        <article id="Crous">
+            <div class="overlay">
+                <h4>Crous</h4>
+                <p>créée à Strasbourg, cette association a maintenant plein de magasins</p>
+                <a class="button-assoc"
+                   href="https://www.instagram.com/p/B2dybNTo4e4/">Plus d'infos</a>
+            </div>
+        </article>
+        <article id="BeeVrac">
+            <div class="overlay">
+                <h4>BeeVrac</h4>
+                <p>créée à Strasbourg, cette association a maintenant plein de magasins</p>
+                <a class="button-assoc"
+                   href="https://beevrac.fr/">Plus d'infos</a>
+            </div>
+        </article>
+        <article id="RepairCafe">
+            <div class="overlay">
+                <h4>Repair Café</h4>
+                <p>créée à Strasbourg, cette association a maintenant plein de magasins</p>
+                <a class="button-assoc"
+                   href="http://www.repaircafe-strasbourg.fr/">Plus d'infos</a>
+            </div>
+        </article>
+    </div>
+</section>
+
+
+
+
+
+
+
+<h2 id="Dechets">Gestion des déchets</h2>
+
+<section class="dechets">
+
+    <h5>Conseils</h5>
+
+    <article class="informations">
+        <img alt="logo recyclage" src="https://cdn.pixabay.com/photo/2018/04/05/23/59/nature-3294632_960_720.jpg">
+        <div class="conseils">
+            <ul class="point">
+                <li>Le recyclage commence déjà par un tri sélectif de nos ordures ménagères et de nos déchets : piles, cartouche d'encre, papier, verre, plastique en tenant compte de différentes consignes.</li>
+                <li>Je peux aussi déposer mes déchets et mes encombrants à la déchetterie proche de chez moi, comme : les restes de peinture, de solvants usagés ou d'huiles usagées.</li>
+                <li>Je proscris les dépôts sauvages pour préserver mon environnement.</li>
+                <li>Je rapporte à mon pharmacien les médicaments non utilisés ou périmés ainsi que leurs emballages.</li>
+                <li>Rapporter mes objets dont je n'ai plus l'utilité ou arrivant en fin de vie permet de recycler les matières qui les composent.</li>
+            </ul>
+        </div>
+    </article>
+    <h5>Points de collecte</h5>
+
+    <article class="descriptions">
+        <img alt="Poubelle" src="https://cdn.pixabay.com/photo/2013/11/09/20/19/trash-207996_960_720.jpg">
+        <p>jgrjjrehguresnof,ksreng,jselgtfnbslrjeqk,nvhjdkrhyjvsuyssefze v' tg e( g(er hg(r (z sthg(h s hstrhs gq q,sdc,uhynisjut</p>
+    </article>
+
+</section>
+</body>
+</html>
