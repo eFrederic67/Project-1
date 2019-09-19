a4367ea1817d5c4d5b7cf798f30e0834c519cbb8
Frederic Egele <frederic.egele.67@gmail.com>
2019-09-19 13:22:58 +0200

    modif meta

--- crous.php
+++ crous.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name=“viewport” content=“width=device-width, initial-scale=1.0, shrink-to-fit=no”>
     <link rel="stylesheet" type="text/css" href="styles.css">
     <link href="https://fonts.googleapis.com/css?family=Poiret+One&display=swap" rel="stylesheet">
     <title>My Green City - Crous</title>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name=“viewport” content=“width=device-width, initial-scale=1.0, shrink-to-fit=no”>
     <link rel="stylesheet" href="./styles.css">
     <link href="https://fonts.googleapis.com/css?family=Poiret+One&display=swap" rel="stylesheet">
     <title>My green city</title>
